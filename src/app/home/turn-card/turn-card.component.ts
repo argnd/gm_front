a441bf208f140aedc6180a55271822589849a13a
armel <gandour37@gmail.com>
2026-08-26 20:59:03 +0200

    changed main card component logic

--- src/app/home/turn-card/turn-card.component.ts
+++ src/app/home/turn-card/turn-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, signal } from '@angular/core';
 import { Turn } from '../../models/turn.model';
 
 @Component({
@@ -6,7 +6,20 @@ import { Turn } from '../../models/turn.model';
   templateUrl: './turn-card.component.html',
   styleUrl: './turn-card.component.scss',
 })
-export class TurnCardComponent {
+export class TurnCardComponent implements OnChanges {
   @Input({ required: true }) turn!: Turn;
   @Input() index = 0;
+  @Input() isLast = false;
+
+  collapsed = signal(true);
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['isLast']) {
+      this.collapsed.set(!this.isLast);
+    }
+  }
+
+  toggle(): void {
+    this.collapsed.update(v => !v);
+  }
 }

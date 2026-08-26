74d13cdb674feb0ddd3550ad92dd99a12daeabf0
armel <gandour37@gmail.com>
2026-08-26 17:39:49 +0200

    test deployment conf sftp

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  googleClientId: '448081264026-fhijmv5fjshr6v1n97lre3ipbkko6b4c.apps.googleusercontent.com',
-  apiBaseUrl: 'https://argnd-iam-secured-backend-448081264026.us-central1.run.app',
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   googleClientId: '448081264026-fhijmv5fjshr6v1n97lre3ipbkko6b4c.apps.googleusercontent.com',
   apiBaseUrl: 'https://argnd-iam-secured-backend-448081264026.us-central1.run.app',
+  // apiBaseUrl: 'http://localhost:8080',
 };
 
